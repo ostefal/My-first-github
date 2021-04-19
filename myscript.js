4a7fb00f45df851e2e82d98d27fffb97f968bace
Ostefal <ostefalex@gmail.com>
2021-04-19 23:23:42 +0300

    1

--- myscript.js
+++ myscript.js
@@ -1,7 +1,28 @@
-var isWeekend = false;
-var hadShower = true;
-var hasApple = false;
-var hasOrange = true;
 
-var shouldGoToSchool = !isWeekend && hadShower && (hasApple || hasOrange);
-console.log(shouldGoToSchool);
+hiteRestriction = 150;
+height = prompt("Какого ты роста?");
+
+if (height >= hiteRestriction) {
+    alert("Проходи кататься на этих сумашедших горках!!!")
+}
+else {
+    alert("Тебе надо подрасти =,(((( ")
+}
+
+
+document.write(height);
+var body = document.querySelector('body');
+body.appendChild(document.createElement('div'));
+document.write(hiteRestriction);
+var body = document.querySelector('body');
+body.appendChild(document.createElement('div'));
+document.write(hiteRestriction);
+var body = document.querySelector('body');
+body.appendChild(document.createElement('div'));
+// document.write();
+// var body = document.querySelector('body');
+// body.appendChild(document.createElement('div'))
+
+
+
+// transformTrue = (true == true) ? "Правда" : "Ложь";
