6a9d31a139f0c4c66853bf5d09c1ec6ccb2d3c41
Ostefal <ostefalex@gmail.com>
2021-04-26 23:53:19 +0300

    1

--- JS для детей/JS для детей.js
+++ JS для детей/JS для детей.js
@@ -103,8 +103,11 @@ true
 
     // Сравнение чисел с помощью будевых значений
 
-Ограничение роста на отракционе
+    == - сравнивает независимо от типа (практически равно, так же 0 == false, 1 == true? "5"==5)
+    ===  сравнивает с проверкой типа (разные типы всегда будут false)
 
+
+// Ограничение роста на отракционе
 hiteRestriction = 150;
 height = prompt("Какого ты роста?");
 
@@ -115,3 +118,37 @@ else {
     alert("Тебе надо подрасти =,(((( ")
 }
 
+// Ребята угадывают загаданное число
+
+var mySecretNumber;
+mySecretNumber = prompt("Напиши загаданное число");
+var numberChico = Math.floor(Math.random()*10)+1;
+var numberHarpo = Math.floor(Math.random()*10)+1;
+var namberGroucho = Math.floor(Math.random()*10)+1;
+
+if (mySecretNumber == numberChico) {
+    alert("Чико угадал число " + mySecretNumber);
+} else if (mySecretNumber == numberHarpo) {
+    alert("Харпо уuадал число "+mySecretNumber);
+} else if (mySecretNumber == namberGroucho) {
+    alert("Гручо угадал число "+mySecretNumber);
+} else {
+    alert ("Никто не угадал твоё число!)))");
+}
+
+
+// // Правила входа в кинотеатр. Возраст 12+ или совместно с родителями   КОД НЕ РАБОТАЕТ. НУЖНО ФИКСИТЬ КОМАНДУ prompt. Сейчас prompt присваивает одно и тоже значение двум переменным. КОД НЕ РАБОТАЕТ.
+
+var age = prompt("Какой у тебя возраст?");
+var accompanied = prompt("Ты пришёл в сопровождении взрослых?");
+
+document.write(age);
+console.log(age);
+
+if (age => 12) {
+    alert("Приятного просмотра, проходите на ваши места.");
+// }else if (accompanied == "да") {
+//     alert("Приятного просмотра, проходите на ваши места.");
+}else {
+    alert("Малышь, тебе нужно подрасти или придти со взрослым");
+}
---
+++ JS для детей/myscript.js
@@ -0,0 +1,63 @@
+// Пробел
+// document.write(height);
+// var body = document.querySelector('body');
+// body.appendChild(document.createElement('div'));
+
+// Ввод значений через всплывающее окно
+// переменная = prompt("Какого ты роста?");
+
+// IF ELSE IF ELSE IF ELSE 
+// if (x > 11) {
+//     x += y;
+//   } else if (x == 4) { 
+//     x -= y; 
+//   } else { 
+//     x = y;
+//   }
+
+// ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ ПРОГРАММЫ
+
+// Проверка роста на отракционе // Проверка роста на отракционе // Проверка роста на отракционе // Проверка роста на отракционе // Проверка роста на отракционе // Проверка роста на отракционе 
+// hiteRestriction = 150;
+// height = prompt("Какого ты роста?");
+
+// if (height >= hiteRestriction) {
+//     alert("Проходи кататься на этих сумашедших горках!!!")
+// }
+// else {
+//     alert("Тебе надо подрасти =,(((( ")
+// }
+
+// Ребята угадывают загаданное число// Ребята угадывают загаданное число// Ребята угадывают загаданное число// Ребята угадывают загаданное число// Ребята угадывают загаданное число
+// var mySecretNumber;
+// mySecretNumber = prompt("Напиши загаданное число");
+// var numberChico = Math.floor(Math.random()*10)+1;
+// var numberHarpo = Math.floor(Math.random()*10)+1;
+// var namberGroucho = Math.floor(Math.random()*10)+1;
+
+// if (mySecretNumber == numberChico) {
+//     alert("Чико угадал число " + mySecretNumber);
+// } else if (mySecretNumber == numberHarpo) {
+//     alert("Харпо уuадал число "+mySecretNumber);
+// } else if (mySecretNumber == namberGroucho) {
+//     alert("Гручо угадал число "+mySecretNumber);
+// } else {
+//     alert ("Никто не угадал твоё число!)))");
+// }
+
+// // Правила входа в кинотеатр. Возраст 12+ или совместно с родителями   КОД НЕ РАБОТАЕТ. НУЖНО ФИКСИТЬ КОМАНДУ prompt. Сейчас prompt присваивает одно и тоже значение двум переменным. КОД НЕ РАБОТАЕТ.
+
+// var age = prompt("Какой у тебя возраст?");
+// var accompanied = prompt("Ты пришёл в сопровождении взрослых?");
+
+// document.write(age);
+// console.log(age);
+
+// if (age => 12) {
+//     alert("Приятного просмотра, проходите на ваши места.");
+// // }else if (accompanied == "да") {
+// //     alert("Приятного просмотра, проходите на ваши места.");
+// }else {
+//     alert("Малышь, тебе нужно подрасти или придти со взрослым");
+// }
+
