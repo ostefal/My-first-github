77841850bd55c50ccb585d780d8b36a0275ef69f
OstefaL <ostefalex@gmail.com>
2022-02-10 13:05:51 +0300

    1

--- JS c нуля Samurai/index.js
+++ JS c нуля Samurai/index.js
@@ -7,3 +7,33 @@
 
 // Урок 02.
 
+// var str1;
+// str1 = "number1";
+
+// window.alert(str1);
+// window.alert("Number 2");
+// window.alert("Number 3");
+
+// var wordCat;
+// wordCat = "Cat";
+
+// var translateOfWordCat;
+// translateOfWordCat = "Кот";
+
+// window.alert(wordCat);
+// window.alert(translateOfWordCat);
+
+// Урок 8
+
+var helloMessageElement;
+
+var helloMessageId = "hello-massage"
+helloMessageElement = document.getElementById(helloMessageId);
+
+window.alert(helloMessageElement.value);
+window.alert(helloMessageElement.id);
+
+// var hello;
+// hello = "Hello";
+
+// window.alert(hello);
