a8541db3e69141f11795e43c7b3ab4aff155ed92
OstefaL <ostefalex@gmail.com>
2022-02-17 18:25:47 +0300

    1

--- JS c нуля Samurai/index.js
+++ JS c нуля Samurai/index.js
@@ -34,13 +34,37 @@ LastName = document.getElementById("LastName_Id")
 var Address;
 Address = document.getElementById("Address_Id")
 
-window.alert(firstName_El.value);
-window.alert(LastName.value);
-window.alert(Address.value);
+// window.alert(firstName_El.value);
+// window.alert(LastName.value);
+// window.alert(Address.value);
 
 
-var Sity_list;
-Sity_list = document.getElementById("Sity list");
+var Sity_list = document.getElementById("Sity list");
 
-console.log(Sity_list.value);
-window.alert(Sity_list.value);
+// console.log(Sity_list.value);
+// window.alert(Sity_list.value);
+
+
+myHobby = document.getElementById("myHobby_ID");
+// window.alert(myHobby.value);
+
+var myPhoto = document.getElementById("myPhoto_ID")
+// window.alert(myPhoto_ID.value);
+
+var myPhotoPic = document.getElementById("myPhotoPic")
+
+// window.alert(myPhoto_ID.innerHTML);
+// console.log(myPhoto_ID.innerHTML);
+
+// window.alert(myHobby_ID.innerHTML);
+// console.log(myHobby_ID.innerHTML);
+
+// window.alert(Sity_list.innerHTML);
+// console.log(Sity_list.innerHTML);
+
+// window.alert(myPhotoPic.src);
+// console.log(myPhotoPic.alt);
+// console.log(myPhotoPic.src);
+
+firstName_El.value = "Смена имени";
+LastName.value = "Смена фамилии";
