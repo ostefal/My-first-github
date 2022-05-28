a669777a887d62bf7a9f1fe6ace5588e1e5befc9
OstefaL <ostefalex@gmail.com>
2022-05-28 09:05:27 +0300

    2

---
+++ Проект для экспериментов/script.js
@@ -0,0 +1,25 @@
+var arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var chisla = [
+    "1",
+    "2",
+    "3",
+    "R",
+    "R",
+];
+
+var simvol = 
+
+dinosaurs[simvol] = "другой динозавр";
+
+// function ssilka() {
+//     var bukva = 
+//     
+abab = "https://my-calend.ru/calendar/"+ chisla;
+//     return;
+// }
+
+// <a href="ssilka">Ёу</a>
+
+
