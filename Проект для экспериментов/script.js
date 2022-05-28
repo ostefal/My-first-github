436259dab68f835eaf9341d505fed7406287dbee
OstefaL <ostefalex@gmail.com>
2022-05-28 10:09:32 +0300

    1

--- Проект для экспериментов/script.js
+++ Проект для экспериментов/script.js
@@ -1,25 +1,62 @@
-var arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 1,2,3,4,5,6,7,8,9,0];
+var simvol
+var perebor 
 
-var chisla = [
-    "1",
-    "2",
-    "3",
-    "R",
-    "R",
-];
+for (i = 0; i < arr_en.length; i++) {
+    simvol = arr_en[i];
+    perebor = "U"+"w"+simvol+"RR";
+    // window.open('https://my-calend.ru/calendar/'+ perebor);
+    window.open('https://my-calend.ru/calendar/'+ perebor, '_blank');
+    console.log(perebor);
+}
 
-var simvol = 
 
-dinosaurs[simvol] = "другой динозавр";
 
-// function ssilka() {
-//     var bukva = 
-//     
-abab = "https://my-calend.ru/calendar/"+ chisla;
-//     return;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ssilka = "https://my-calend.ru/calendar/"+ simvol;
+
+// // var arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+// var chisla = [
+//     "1",
+//     "2",
+//     "3",
+//     "R",
+//     "R",
+// ];
+
+// var simvol = 
+
+// dinosaurs[simvol] = "другой динозавр";
+
+// // function ssilka() {
+// //     var bukva = 
+// //     
+// abab = "https://my-calend.ru/calendar/"+ chisla;
+// //     return;
+// // }
+
+// // <a href="ssilka">Ёу</a>
+
+
+// var Hello = 30;
+// for (i=0; i < Hello; i++) {
+//     console.log("Привет");
 // }
-
-// <a href="ssilka">Ёу</a>
-
 
