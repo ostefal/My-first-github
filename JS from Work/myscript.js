1e25239de1f80f3ede6ce8aec983b57693923205
OstefaL <ostefalex@gmail.com>
2022-10-03 09:16:05 +0300

    1

--- JS from Work/myscript.js
+++ JS from Work/myscript.js
@@ -1,8 +1,13 @@
-var name = "Joe";
-var i = 0;
-while (i < 2) {
-document.write("Happy Birthday to you.<br>");
-i = i + 1;
-}
-document.write("Happy Birthday dear " + name + ",<br>");
-document.write("Happy Birthday to you.<br>"); 
+var word = "bottles";
+var count = 99;
+while (count > 0) {
+    document.write(count + " " + word + " of beer on the wall. <br>");
+    document.write(count + " " + word + " of beer,<br>");
+    document.write("Take one down, pass it around,<br>");
+    count = count - 1;
+    if (count > 0) {
+        document.write(count + " " + word + " of beer on the wall.<br>");
+    } else {
+        document.write("No more " + word + " of beer on the wall.<br>");
+    }
+}    
