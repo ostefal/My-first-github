9f3fc9ba6093bba6003faf2f6f4a355e4d7cc520
OstefaL <ostefalex@gmail.com>
2022-10-04 12:58:24 +0300

    Update tutorial.js

--- JS from Work/tutorial.js
+++ JS from Work/tutorial.js
@@ -76,3 +76,21 @@ document.write("Happy Birthday to you.<br>");
             </script>
         </body>
     </html>
+
+
+{/* // <script> */}
+var word = "bottles";
+var count = 99;
+while (count > 0) {
+    document.write(count + " " + word + " of beer on the wall. <br>");
+    document.write(count + " " + word + " of beer,<br>");
+    document.write("Take one down, pass it around,<br>");
+    count = count - 1;
+    if (count > 0) {
+        document.write(count + " " + word + " of beer on the wall.<br>");
+    } else {
+        document.write("No more " + word + " of beer on the wall.<br>");
+    }
+}
+// </script>
+
